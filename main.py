a26c23d5dcbe6dfa819433b5b2bf8ec4eb7b8d07
quant1on <ilyusha.k06@gmail.com>
2025-01-11 21:40:47 +0300

    implemented dichotomy method

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ import numpy
 import typing as tp
 
 def dichotomy_search(a: float, b: float, f: tp.Callable, err: float) -> float:
-    pass
-
+    epsilon = err / 2
+    while (b - a) / 2 > err:
+        y_k = (a + b - epsilon) / 2
+        z_k = (a + b + epsilon) / 2
+        if f(y_k) <= f(z_k):
+            b = z_k
+        else:
+            a = y_k
+    return (a + b) / 2
--- tests/test_dichotomy.py
+++ tests/test_dichotomy.py
@@ -8,39 +8,39 @@ class GetDichotomyTestCase(unittest.TestCase):
         
         self.assertAlmostEqual(first=dichotomy_search(a=-2, b=5,
                                 f=func.func_test_dichotomy_1, err=10e-7),
-                                second=0.0, places=6)
+                                second=0.0, delta=10e-5)
         
-        self.assertAlmostEqual(first=dichotomy_search(a=-10, b=4,
+        self.assertAlmostEqual(first=dichotomy_search(a=-10, b=1.5,
                                 f=func.func_test_dichotomy_2, err=10e-7),
-                                second=0.0, places=6)
+                                second=0.0, delta=10e-5)
 
         self.assertAlmostEqual(first=dichotomy_search(a=-3, b=6,
                                 f=func.func_test_dichotomy_3, err=10e-7),
-                                second=0.0, places=6)
+                                second=0.0, delta=10e-5)
     def test_left_overlap(self):
 
         self.assertAlmostEqual(first=dichotomy_search(a=0, b=5,
                                 f=func.func_test_dichotomy_1, err=10e-7),
-                                second=0.0, places=6)
+                                second=0.0, delta=10e-5)
         
-        self.assertAlmostEqual(first=dichotomy_search(a=0, b=4,
+        self.assertAlmostEqual(first=dichotomy_search(a=0, b=1.5,
                                 f=func.func_test_dichotomy_2, err=10e-7),
-                                second=0.0, places=6)
+                                second=0.0, delta=10e-5)
 
         self.assertAlmostEqual(first=dichotomy_search(a=0, b=6,
                                 f=func.func_test_dichotomy_3, err=10e-7),
-                                second=0.0, places=6)
+                                second=0.0, delta=10e-5)
 
     def test_right_overlap(self):
 
         self.assertAlmostEqual(first=dichotomy_search(a=-2, b=0,
                                 f=func.func_test_dichotomy_1, err=10e-7),
-                                second=0.0, places=6)
+                                second=0.0, delta=10e-5)
         
         self.assertAlmostEqual(first=dichotomy_search(a=-10, b=0,
                                 f=func.func_test_dichotomy_2, err=10e-7),
-                                second=0.0, places=6)
+                                second=0.0, delta=10e-5)
 
         self.assertAlmostEqual(first=dichotomy_search(a=-3, b=0,
                                 f=func.func_test_dichotomy_3, err=10e-7),
-                                second=0.0, places=6)
+                                second=0.0, delta=10e-5)
