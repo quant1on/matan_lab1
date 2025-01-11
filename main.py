6be2443980c70a2ad14e83fb60aff16a8828e359
quant1on <ilyusha.k06@gmail.com>
2025-01-11 14:12:27 +0300

    init commit lol

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy
+import typing as tp
+
+def golden_curve_method(a: float, b: float, f: tp.Callable) -> float:
+    pass
+
---
+++ tests/test_goldencurve.py
@@ -0,0 +1,13 @@
+from main import golden_curve_method
+import unittest
+
+class GetGoldenCurveTestCase(unittest.TestCase):
+
+    def test_regular_case(self):
+        pass
+
+    def test_left_overlap(self):
+        pass
+
+    def test_right_overlap(self):
+        pass
